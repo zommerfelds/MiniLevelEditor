ae1e00d8ff1801428e34adfe5e3d87ea4a48eddb
Christian Zommerfelds <christian.zommerfelds@gmail.com>
2024-03-03 22:02:50 +0100

    Offset image in tile picker for each tile

--- client/src/components/TileSelector.vue
+++ client/src/components/TileSelector.vue
@@ -4,15 +4,24 @@ import { useWorldStore } from '@/stores/world'
 
 const tools = useToolsStore()
 const world = useWorldStore()
+
+const tileSize = 16
+const imageSize = 64
+
+const iconSize = 22
+const backgroundSize = imageSize * (iconSize / tileSize)
 </script>
 
 <template>
+  <!-- TODO: use tiles from config like in LevelContent.vue -->
   <div class="container" v-for="i in 4" :key="i">
     <div class="row pb-1" @click="tools.selectedTile = i">
       <div class="col">
         <a href="#" class="tile-selector" :class="tools.selectedTile == i ? 'tile-selected' : ''">
-          <!-- img :src="'api/tilesets/tileset1.png'" class="pe-2 tile-img" / -->
-          <div class="tile-icon pixelart"></div>
+          <div
+            class="tile-icon pixelart"
+            :style="'background-position: ' + iconSize * (-i + 1) + 'px 0px;'"
+          ></div>
           <span class="ps-2">Tile {{ i }}</span>
         </a>
       </div>
@@ -30,17 +39,11 @@ const world = useWorldStore()
   color: rgb(255, 255, 255);
 }
 
-.tile-img {
-  width: 2.5em;
-  clip-path: inset(0px 0px 16px 16px);
-}
-
 .tile-icon {
   display: inline-block;
   background-image: url('api/tilesets/tileset1.png');
-  background-position: 0px 16px;
-  background-size: 32px;
-  width: 16px;
-  height: 16px;
+  background-size: v-bind(backgroundSize + 'px');
+  width: v-bind(iconSize + 'px');
+  height: v-bind(iconSize + 'px');
 }
 </style>
