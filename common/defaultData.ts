be5f5a55ca717b9bfd0aaa1303b0520a5f288edb
Christian Zommerfelds <christian.zommerfelds@gmail.com>
2024-03-09 12:44:12 +0100

    Add allowedLayers to example data

--- common/defaultData.ts
+++ common/defaultData.ts
@@ -12,14 +12,14 @@ export function makeDefaultData() {
       "layers": [{ "name": "Static" }, { "name": "Entities" }],
       "tiles": [
         { "name": "Empty" },
-        { "name": "T1", "x": 0, "y": 0 },
-        { "name": "T2", "x": 16, "y": 0 },
-        { "name": "T3", "x": 32, "y": 0 },
-        { "name": "T4", "x": 48, "y": 0 },
-        { "name": "Player", "x": 0, "y": 16 },
-        { "name": "Bush1", "x": 16, "y": 16 },
-        { "name": "Bush2", "x": 32, "y": 16 },
-        { "name": "Trunk", "x": 48, "y": 16 }
+        { "name": "T1", "x": 0, "y": 0, "allowedLayers": [0] },
+        { "name": "T2", "x": 16, "y": 0, "allowedLayers": [0] },
+        { "name": "T3", "x": 32, "y": 0, "allowedLayers": [0] },
+        { "name": "T4", "x": 48, "y": 0, "allowedLayers": [0] },
+        { "name": "Player", "x": 0, "y": 16, "allowedLayers": [1] },
+        { "name": "Bush1", "x": 16, "y": 16, "allowedLayers": [1] },
+        { "name": "Bush2", "x": 32, "y": 16, "allowedLayers": [1] },
+        { "name": "Trunk", "x": 48, "y": 16, "allowedLayers": [1] }
       ],
     },
     levels: [makeDefaultLevel()],
