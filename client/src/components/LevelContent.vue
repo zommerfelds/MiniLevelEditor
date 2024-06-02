cc7e9491e731f414457f44219d98d6e2e5c04ec3
Christian Zommerfelds <christian.zommerfelds@gmail.com>
2024-06-02 12:28:26 +0200

    Add cursors

--- client/src/components/LevelContent.vue
+++ client/src/components/LevelContent.vue
@@ -1,6 +1,9 @@
 <script setup lang="ts">
-import { onMounted, onUnmounted } from 'vue'
+import { computed, onMounted, onUnmounted } from 'vue'
 import { launch } from '@/phaser/phaser-main'
+import { Tool, useToolsStore } from '@/stores/tools'
+
+const tools = useToolsStore()
 
 onMounted(() => {
   const phaserInstance = launch('phaser-game')
@@ -8,8 +11,25 @@ onMounted(() => {
     phaserInstance.destroy(true)
   })
 })
+
+const cursor = computed(() => {
+  switch (tools.selectedTool) {
+    case Tool.Select:
+      return 'crosshair'
+    case Tool.Move:
+      return 'move'
+    default:
+      return 'default'
+  }
+})
 </script>
 
 <template>
-    <div id="phaser-game" class="w-100" style="overflow: hidden" @contextmenu.prevent=""></div>
+  <div
+    id="phaser-game"
+    class="w-100"
+    style="overflow: hidden"
+    :style="{ cursor: cursor }"
+    @contextmenu.prevent=""
+  ></div>
 </template>
