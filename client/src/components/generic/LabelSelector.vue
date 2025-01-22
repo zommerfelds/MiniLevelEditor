4165d4607e461e656414ab2970475e94ecf745ab
Christian Zommerfelds <christian.zommerfelds@gmail.com>
2025-01-22 22:55:40 +0100

    Fix styling for type assignment UI

--- client/src/components/generic/LabelSelector.vue
+++ client/src/components/generic/LabelSelector.vue
@@ -13,68 +13,94 @@ const props = defineProps<{
 </script>
 
 <template>
-  <template v-for="(selectedLabel, selectedLabelIndex) in selectedLabels" :key="selectedLabelIndex">
-    <span
-      class="badge rounded-pill ps-3 me-1 bg-secondary d-inline-flex align-items-center justify-content-center"
+  <div class="labels-container">
+    <template
+      v-for="(selectedLabel, selectedLabelIndex) in selectedLabels"
+      :key="selectedLabelIndex"
     >
-      <div class="dropdown">
-        <button
-          class="btn btn-sm btn-secondary p-0"
-          @click="console.log('not implemented')"
-          :id="'dropdown_' + $.uid + '_' + selectedLabelIndex"
-          data-bs-toggle="dropdown"
-          aria-expanded="false"
-        >
-          {{ selectedLabel }}
-        </button>
-        <ul class="dropdown-menu" :aria-labelledby="'dropdown_' + $.uid + '_' + selectedLabelIndex">
-          <template v-for="(label, labelIndex) in props.labels" :key="labelIndex">
-            <li v-if="label != selectedLabel">
-              <a
-                class="dropdown-item"
-                href="#"
-                @click="selectedLabels[selectedLabelIndex] = label"
-                >{{ label }}</a
-              >
-            </li>
-          </template>
-        </ul>
-      </div>
-      <button
-        href="#"
-        class="btn btn-sm p-0 text-white ms-2"
-        @click="selectedLabels.splice(selectedLabelIndex, 1)"
+      <span
+        class="badge rounded-pill ps-3 me-1 bg-secondary d-inline-flex align-items-center justify-content-center"
       >
-        <FontAwesomeIcon :icon="faTimesCircle" />
+        <div class="dropdown">
+          <button
+            class="btn btn-sm btn-secondary p-0 label-text"
+            :id="'dropdown_' + $.uid + '_' + selectedLabelIndex"
+            data-bs-toggle="dropdown"
+            aria-expanded="false"
+          >
+            {{ selectedLabel }}
+          </button>
+          <ul
+            class="dropdown-menu"
+            :aria-labelledby="'dropdown_' + $.uid + '_' + selectedLabelIndex"
+          >
+            <template v-for="(label, labelIndex) in props.labels" :key="labelIndex">
+              <li v-if="label != selectedLabel">
+                <a
+                  class="dropdown-item"
+                  href="#"
+                  @click="selectedLabels[selectedLabelIndex] = label"
+                  >{{ label }}</a
+                >
+              </li>
+            </template>
+          </ul>
+        </div>
+        <button
+          href="#"
+          class="btn btn-sm p-0 text-white ms-2"
+          @click="selectedLabels.splice(selectedLabelIndex, 1)"
+        >
+          <FontAwesomeIcon :icon="faTimesCircle" />
+        </button>
+      </span>
+    </template>
+    <span class="dropdown ps-1">
+      <button
+        class="btn text-secondary plus-button"
+        data-bs-toggle="dropdown"
+        aria-expanded="false"
+        :id="'dropdown_' + $.uid + '_add'"
+      >
+        <FontAwesomeIcon :icon="faSquarePlus" />
       </button>
+      <ul class="dropdown-menu" :aria-labelledby="'dropdown_' + $.uid + '_add'">
+        <template v-for="(label, labelIndex) in props.labels" :key="labelIndex">
+          <li v-if="!selectedLabels?.includes(label)">
+            <a class="dropdown-item" href="#" @click="selectedLabels.push(label)">{{ label }}</a>
+          </li>
+        </template>
+      </ul>
     </span>
-  </template>
-  <span class="p-1"></span>
-  <span class="dropdown align-text-top">
-    <button
-      class="btn text-secondary p-0"
-      data-bs-toggle="dropdown"
-      aria-expanded="false"
-      :id="'dropdown_' + $.uid + '_add'"
-    >
-      <FontAwesomeIcon :icon="faSquarePlus" />
-    </button>
-    <ul class="dropdown-menu" :aria-labelledby="'dropdown_' + $.uid + '_add'">
-      <template v-for="(label, labelIndex) in props.labels" :key="labelIndex">
-        <li v-if="!selectedLabels?.includes(label)">
-          <a class="dropdown-item" href="#" @click="selectedLabels.push(label)">{{ label }}</a>
-        </li>
-      </template>
-    </ul>
-  </span>
+  </div>
 </template>
 
 <style scoped>
+.labels-container {
+  display: flex;
+  flex-wrap: wrap;
+  row-gap: 0.2em;
+}
+
+.plus-button {
+  padding-left: 0;
+  padding-right: 0;
+  padding-bottom: 0;
+  padding-top: 0.1em;
+}
+
 .dropdown-fill {
   display: flex;
   justify-content: space-between;
   align-items: center;
   width: 100%;
   text-align: left;
+}
+
+.label-text {
+  max-width: 14em;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  overflow: hidden;
 }
 </style>
